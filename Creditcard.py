9e153edf38e547094cab70b62f0c321d4713a4a8
Abdul Haseeb <127509119+abdulhaseeb1308@users.noreply.github.com>
2023-07-13 20:39:35 +0530

    Add files via upload

---
+++ Creditcard.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import SMOTE
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+# Load the dataset
+data = pd.read_csv('creditcard.csv')
+
+# Preprocessing and normalization
+scaler = StandardScaler()
+data['NormalizedAmount'] = scaler.fit_transform(data['Amount'].values.reshape(-1, 1))
+data = data.drop(['Time', 'Amount'], axis=1)
+
+# Split the dataset into features and target
+X = data.drop('Class', axis=1)
+y = data['Class']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Handle class imbalance using oversampling with SMOTE
+smote = SMOTE(random_state=42)
+X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
+
+# Train the model
+model = LogisticRegression(solver='liblinear', max_iter=1000)
+model.fit(X_train_resampled, y_train_resampled)
+
+# Evaluate the model
+y_pred = model.predict(X_test)
+print(classification_report(y_test, y_pred))
+
+
